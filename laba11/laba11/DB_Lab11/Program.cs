6774a90957569a356b615b376e3847434677c010
nastyaseptilko <anastasiyaseptilko@mail.ru>
2020-06-05 00:22:43 +0300

    finish labs

---
+++ laba11/laba11/DB_Lab11/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace DB_Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            zz:
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("1 | - Вывести список клиентов магазина");
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("2 | - Удалить клиента из списка");
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("3 | - Изменить клиента из списка");
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("4 | - Добавить клиента из списка");
+            Console.WriteLine("__________________________________________");
+
+
+            int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Select();
+                        break;
+                    case 2:
+                        Delete();
+                        break;
+                    case 3:
+                        Update();
+                        break;
+                    case 4:
+                        Insert();
+                        break;
+                    default:
+                        Console.WriteLine("default");
+                        break;
+                }
+
+            goto zz;
+
+            void Insert()
+            {
+                Console.Write("Добавим клиента нашего магазина в БД: ");
+                string cityName = Console.ReadLine();
+
+                using (SQLiteConnection Connect = new SQLiteConnection(@"Data Source=D:\Учёба\3 curs\2 сем\DB\laba11\\Shop.db; Version=3;"))
+                {
+                    string commandText = "INSERT INTO [Clients] ([name]) VALUES(@name)";
+                    SQLiteCommand Command = new SQLiteCommand(commandText, Connect);
+                    Command.Parameters.AddWithValue("@name", cityName);
+                    Connect.Open();
+                    Command.ExecuteNonQuery();
+                    Connect.Close();
+                }
+            }
+
+
+            void Select()
+            {
+                List<string> cityList = new List<string>();
+                using (SQLiteConnection Connect = new SQLiteConnection(@"Data Source=D:\Учёба\3 curs\2 сем\DB\laba11\\Shop.db; Version=3;"))
+                {
+                    try
+                    {
+                        Connect.Open();
+                        SQLiteCommand Command = new SQLiteCommand
+                        {
+                            Connection = Connect,
+                            CommandText = @"SELECT * FROM Clients"
+                        };
+                        SQLiteDataReader sqlReader = Command.ExecuteReader();
+                        string _dbname = null;
+                       // string _dbDescript = null;
+                        string _dbId = null;
+                        while (sqlReader.Read()) 
+                        {
+                            _dbname = sqlReader["name"].ToString();
+                            //_dbDescript = sqlReader["description"].ToString();
+                            _dbId = sqlReader["id"].ToString();
+                            cityList.Add(_dbId + " " + _dbname);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        Connect.Close();
+                    }
+                }
+
+                foreach (var list in cityList)
+                    Console.WriteLine(list);
+            }
+            
+
+
+            void Update()
+            {
+                using (SQLiteConnection Connect = new SQLiteConnection(@"Data Source=D:\Учёба\3 curs\2 сем\DB\laba11\\Shop.db; Version=3;"))
+                {
+                    string commandText = "UPDATE [Clients] SET [name] = @value WHERE [id] = @id";
+                    SQLiteCommand Command = new SQLiteCommand(commandText, Connect);
+                    Console.WriteLine("Введите id клиента для замены");
+                    var id = Console.ReadLine();
+                    Console.WriteLine("Введите новое имя клиента");
+                    var city = Console.ReadLine();
+                    Command.Parameters.AddWithValue("@value", city);
+                    Command.Parameters.AddWithValue("@id", id);
+                    Connect.Open();
+                    Int32 _rowsUpdate = Command.ExecuteNonQuery();
+                    Console.WriteLine("Обновлено строк: " + _rowsUpdate);
+                    Connect.Close();
+                }
+            }
+
+            void Delete()
+            {
+                using (SQLiteConnection Connect = new SQLiteConnection(@"Data Source=D:\Учёба\3 curs\2 сем\DB\laba11\\Shop.db; Version=3;"))
+                {
+                    string commandText = "DELETE FROM [Clients] WHERE [id] = @id";
+                    SQLiteCommand Command = new SQLiteCommand(commandText, Connect);
+                    Console.WriteLine("Введите id для удаления");
+                    var id = Console.ReadLine();
+                    Command.Parameters.AddWithValue("@id", id);
+                    Connect.Open();
+                    Command.ExecuteNonQuery();
+                    Connect.Close();
+                }
+            }
+        }
+    }
+}
---
+++ laba11/laba11/DB_Lab11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DB_Lab11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DB_Lab11")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b10dd8c6-fc20-473a-8015-eef7cc5f00fe")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
